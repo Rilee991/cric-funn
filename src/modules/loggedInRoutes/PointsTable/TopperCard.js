c1a51e7df38031c55b974bae05b822c349dad5dd
cypher33 <rohitkumar25.rkl@gmail.com>
2023-06-01 17:24:32 +0530

    topper card complete

---
+++ src/modules/loggedInRoutes/PointsTable/TopperCard.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Badge, Avatar, Grid, Card, CardActionArea, CardContent, GridList, Collapse } from '@material-ui/core';
+
+import backGround from '../../../images/stats.jpg';
+
+const TopperCard = () => {
+    return (
+        <Card style={{ boxShadow: "5px 5px 20px", backgroundImage: `url(${backGround})`, backgroundRepeat:"no-repeat", backgroundSize: "inherit",height: "auto", backgroundBlendMode: "hard-light" }} className="tw-mt-2 tw-mb-10 xl:tw-w-[70%] md:tw-w-[90%] tw-rounded-[40px]">
+			<CardActionArea style={{ backgroundSize: "contain", backgroundImage: `url(${"https://www.freepnglogos.com/uploads/1-number-png/number-1-rankings-georgia-tech-40.png"})`, backgroundRepeat: "no-repeat" }}>
+				<CardContent className="tw-flex tw-text-white tw-items-center tw-justify-evenly sm:tw-h-[180px] tw-h-[100px]">
+					<div>
+						<img className="tw-w-[85px] tw-h-[85px] sm:tw-w-[150px] sm:tw-h-[150px]" src={"https://scores.iplt20.com/ipl/playerimages/Shubman%20Gill.png"} />
+					</div>
+					<Typography className="tw-flex tw-items-center tw-gap-2 sm:tw-text-[25px]" variant={"button"} style={{fontSize: 15}} component="p">
+						<div className="tw-flex tw-gap-2">
+							<div className="tw-flex tw-flex-col tw-items-center">
+								<div>
+									<b><i>Bets</i></b>
+								</div>
+								<div>
+									<b><i>74</i></b>
+								</div>
+							</div>
+							<div className="tw-flex tw-flex-col tw-items-center">
+								<div>
+									<b><i>Wins</i></b>
+								</div>
+								<div>
+									<b><i>44</i></b>
+								</div>
+							</div>
+							<div className="tw-flex tw-flex-col tw-items-center">
+								<div>
+									<b><i>Win %</i></b>
+								</div>
+								<div>
+									<b><i>62.32</i></b>
+								</div>
+							</div>
+							<div className="tw-flex tw-flex-col tw-items-center">
+								<div>
+									<b><i>Points</i></b>
+								</div>
+								<div>
+									<b><i>120000</i></b>
+								</div>
+							</div>
+						</div>
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+    );
+}
+
+export default TopperCard;
