d5c1f2f21c2dbff2c767dab3a434360f0c064abf
cypher33 <rohitkumar25.rkl@gmail.com>
2023-06-11 22:51:29 +0530

    notifs cleanup

--- src/modules/loggedInRoutes/Notifications/Notifications.js
+++ src/modules/loggedInRoutes/Notifications/Notifications.js
@@ -27,20 +27,17 @@ const Notifications = (props) => {
                 </div>
                 <Divider className="tw-w-full tw-my-2" />
                 <div className="tw-w-full"> {
-                    notifications.length ? notifications.map((notification, idx) => {
-                        console.log("came " );
-                        return (
-                            <Card key={idx} className="tw-rounded-[40px]" style={{width: '100%', marginBottom: "20px", background: notification.isNoResult ? "linear-gradient(rgb(123 16 199), black)" : notification.betWon ? "linear-gradient(rgb(73 199 16), black)" : "linear-gradient(180deg, #b21616, black)", color: "aliceblue" }}>
-                                <CardActionArea>
-                                    <CardContent>
-                                        <Typography variant={"button"} style={{fontSize: 13, gap: "4px", color: "aliceblue", display: "flex", justifyContent: "center", alignItems: "center"}} component="p">
-                                            {notification.isNoResult ? <SentimentSatisfied /> : notification.betWon ? <SentimentVerySatisfied /> : <SentimentVeryDissatisfied />} <b>{notification.body}</b>
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        )
-                    }) :
+                    notifications.length ? notifications.map((notification, idx) => (
+                        <Card key={idx} className="tw-rounded-[40px]" style={{width: '100%', marginBottom: "20px", background: notification.isNoResult ? "linear-gradient(rgb(123 16 199), black)" : notification.betWon ? "linear-gradient(rgb(73 199 16), black)" : "linear-gradient(180deg, #b21616, black)", color: "aliceblue" }}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography variant={"button"} style={{fontSize: 13, gap: "4px", color: "aliceblue", display: "flex", justifyContent: "center", alignItems: "center"}} component="p">
+                                        {notification.isNoResult ? <SentimentSatisfied /> : notification.betWon ? <SentimentVerySatisfied /> : <SentimentVeryDissatisfied />} <b>{notification.body}</b>
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    )) :
                     <Alert classes={{ standardSuccess: classes.infoAlertSettings, icon: classes.contentColorSettings, message: classes.contentColorSettings }} className="tw-rounded-[40px] tw-flex tw-justify-center tw-items-center tw-text-[aliceblue]">
                         <Typography variant={"button"} style={{fontSize: 15}} component="p">
                             <b>No New Notifications</b>
