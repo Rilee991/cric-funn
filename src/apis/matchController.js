dc10386be4eb3cec55eedf2da23b11cda60253dc
cypher33 <rohitkumar25.rkl@gmail.com>
2023-06-16 22:11:38 +0530

    defining controllers and poster configs

---
+++ src/apis/matchController.js
@@ -0,0 +1,24 @@
+import { db } from "../config";
+import { MATCH_COLLECTION } from "../global/enums";
+
+export const getMatches = async () => {
+    const matches = await db.collection(MATCH_COLLECTION).orderBy("dateTimeGMT").get();
+
+    return matches;
+}
+
+export const updateMatchById = async (id, matchDetails) => {
+    await db.collection(MATCH_COLLECTION).doc(id).update({ ...matchDetails });
+}
+
+export const createMatch = async (id, matchDetails) => {
+    await db.collection(MATCH_COLLECTION).doc(id).set({ ...matchDetails });
+}
+
+export const clearTable = async () => {
+    const records = await db.collection(MATCH_COLLECTION).get();
+    
+    for(const rec of records.docs) {
+        await rec.ref.delete();
+    };
+}
---
+++ src/apis/userController.js
@@ -0,0 +1,31 @@
+import { db } from "../config";
+import { USER_COLLECTION } from "../global/enums";
+
+export const getUserByUsername = async (username) => {
+    const user = await db.collection(USER_COLLECTION).doc(username).get();
+
+    return user;
+}
+
+export const getUserByKey = async (key, value) => {
+    const user = await db.collection(USER_COLLECTION).where(key, "==", value).get();
+
+    return user;
+}
+
+export const createUser = async (username, userDetails) => {
+    const resp = await db.collection(USER_COLLECTION).doc(username).set(userDetails);
+
+    return resp;
+}
+
+export const updateUserByUsername = async (username, userDetails) => {
+    await db.collection(USER_COLLECTION).doc(username).update({ ...userDetails });
+}
+
+export const updateUserByEmail = async (email, userDetails) => {
+    const docsSnapshot = await db.collection(USER_COLLECTION).where("email", "==", email).get();
+    docsSnapshot.docs.forEach(doc => {
+        updateUserByUsername(doc.id, userDetails);
+    });
+}
