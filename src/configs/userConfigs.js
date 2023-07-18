58f4d63a1b82520726e3de562ae612e64dfc4c97
cypher33 <rohitkumar25.rkl@gmail.com>
2023-07-18 22:42:29 +0530

    added configs

--- src/configs/userConfigs.js
+++ src/configs/userConfigs.js
@@ -1,7 +1,8 @@
 export const DEFAULT_USER_PARAMS = {
     PROFILE_PICTURE: "https://firebasestorage.googleapis.com/v0/b/cric-funn.appspot.com/o/defaultImages%2Fpp.png?alt=media&token=2745d072-9783-4505-95ba-bab54aad2d9c&_gl=1*fgupvy*_ga*MTIwMTA5NzI2Ny4xNjY4Njg3MTAz*_ga_CW55HF8NVT*MTY4NjY1NTk4My41My4xLjE2ODY2NTYwNzkuMC4wLjA.",
     STARTING_POINTS: 3500,
-    STARTING_BETS: []
+    STARTING_BETS: [],
+    BIRTHDAY_GIFT_POINTS: 100
 }
 
 export const ALERT_CONFIGS = {
