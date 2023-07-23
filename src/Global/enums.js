647f15d58567f51ad50edae2b3ee7929b1e2ff9e
cypher33 <rohitkumar25.rkl@gmail.com>
2023-07-23 23:38:24 +0530

    naming collection for configs

--- src/Global/enums.js
+++ src/Global/enums.js
@@ -1,3 +1,8 @@
 export const USER_COLLECTION = `users`;
 export const MATCH_COLLECTION = `ipl_matches`;
 export const LEGEND_COLLECTION = `legends`;
+export const CONFIGURATION_COLLECTION = `configurations`;
+export const CONFIGURATION_DOCS = {
+    CREDITS: `CREDITS`,
+    BACKUPS: `BACKUPS`
+};
