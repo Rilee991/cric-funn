b88a67cbca3c4d04f4ed8f37baef80780071dc1a
cypher33 <rohitkumar25.rkl@gmail.com>
2023-07-23 23:39:25 +0530

    credits left ui on sidebar

--- src/modules/loggedInRoutes/SideNavbar/SideNavbar.js
+++ src/modules/loggedInRoutes/SideNavbar/SideNavbar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Divider, SwipeableDrawer, Hidden, List, ListItem, ListItemIcon, ListItemText, makeStyles, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { AttachMoney } from '@material-ui/icons';
+import { sum } from 'lodash';
+
+import { CONFIGURATION_DOCS } from '../../../global/enums';
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
@@ -39,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
 
 
 const SideNavbar = (props) => {
-    const { setIsNavOpen, isNavOpen, navSelected, navItems, setNavSelected, mobileView } = props;
+    const { setIsNavOpen, isNavOpen, navSelected, navItems, setNavSelected, mobileView, configurations } = props;
     const classes = useStyles();
 
     const handleDrawerToggle = () => {
@@ -52,33 +56,52 @@ const SideNavbar = (props) => {
         item.onClick && item.onClick();
     }
 
+    const getRemainingCredits = () => {
+        const credits = sum(Object.values(configurations[CONFIGURATION_DOCS.CREDITS]));
+        return 100-Math.min(100,credits);
+    }
+
     const getDrawerItems = () => (
-        <div className="tw-flex tw-flex-col tw-z-0">
-            <div className="tw-flex tw-justify-center tw-h-[62px] tw-items-center">
-                <Typography style={{fontSize: 18}}>
-                    <b>MENU</b>
-                </Typography>
-            </div>
-            <Divider />
+        <div className="tw-flex tw-flex-col tw-z-0 tw-h-screen tw-justify-between">
             <div>
-                <List>
-                    {navItems.map((item, index) => (
-                        <Link to={item.to} key={item.id} onClick={() => onClickNavItem(item)}>
-                            <ListItem style={{ background: navSelected == item.id ? "darkslateblue" : "", borderRadius: "40px" }} className={`${item.disabled ? "hover:tw-bg-red-800" : "hover:tw-bg-purple-800"} tw-mb-1`} button key={item.name}>
-                                <ListItemIcon className="tw-text-white">
-                                    {item.icon}
-                                </ListItemIcon>
-                                <ListItemText className={item.disabled ? "tw-text-gray-300" : "tw-text-white"}>
-                                    <p className={item.subText ? "tw-leading-4" : ""}>{item.name}</p>
-                                    {item.subText ? <p className="tw-text-sm tw-leading-4 tw-text-gray-400">
-                                        {item.subText}
-                                    </p> : null }
-                                </ListItemText>
-                            </ListItem>
-                        </Link>
-                    ))}
-                </List>
+                <div className="tw-flex tw-justify-center tw-h-[62px] tw-items-center">
+                    <Typography style={{fontSize: 18}}>
+                        <b>MENU</b>
+                    </Typography>
+                </div>
                 <Divider />
+                <div>
+                    <List>
+                        {navItems.map((item, index) => (
+                            <Link to={item.to} key={item.id} onClick={() => onClickNavItem(item)}>
+                                <ListItem style={{ background: navSelected == item.id ? "darkslateblue" : "", borderRadius: "40px" }} className={`${item.disabled ? "hover:tw-bg-red-800" : "hover:tw-bg-purple-800"} tw-mb-1`} button key={item.name}>
+                                    <ListItemIcon className="tw-text-white">
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText className={item.disabled ? "tw-text-gray-300" : "tw-text-white"}>
+                                        <p className={item.subText ? "tw-leading-4" : ""}>{item.name}</p>
+                                        {item.subText ? <p className="tw-text-sm tw-leading-4 tw-text-gray-400">
+                                            {item.subText}
+                                        </p> : null }
+                                    </ListItemText>
+                                </ListItem>
+                            </Link>
+                        ))}
+                    </List>
+                    <Divider />
+                </div>
+            </div>
+            <div>
+                <ListItem style={{ borderRadius: "40px" }} className={`tw-mb-1 tw-cursor-default`} button>
+                    <ListItemIcon className="tw-text-white">
+                        <AttachMoney />
+                    </ListItemIcon>
+                    <ListItemText className={"tw-text-white"}>
+                        <p className="tw-text-sm tw-leading-4 tw-text-gray-400">
+                            Credits Left Today: <b>{getRemainingCredits()}</b>
+                        </p>
+                    </ListItemText>
+                </ListItem>
             </div>
         </div>
     );
