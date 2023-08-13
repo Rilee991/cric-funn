86cfe97fa4cab638f497d51de9dabe99439d5d07
cypher33 <rohitkumar25.rkl@gmail.com>
2023-08-13 23:16:43 +0530

    edited social media

--- src/components/common/SocialMedia.js
+++ src/components/common/SocialMedia.js
@@ -9,10 +9,10 @@ const SocialMedia = () => {
     return (
         <div className="tw-flex tw-items-center tw-justify-center tw-gap-2">
             <div className="tw-cursor-pointer" onClick={() => openWindow(`https://www.facebook.com/StoneCypher33`)}>
-                <Facebook htmlColor="#106be9" fontSize="large" />
+                <Facebook htmlColor="#fff" fontSize="large" />
             </div>
             <div className="tw-cursor-pointer" onClick={() => openWindow(`https://www.linkedin.com/in/rohit-kumar-a92418141/`)}>
-                <LinkedIn htmlColor="#2b55f7" fontSize="large" />
+                <LinkedIn htmlColor="#fff" fontSize="large" />
             </div>
             <div className="tw-cursor-pointer" onClick={() => openWindow(`https://twitter.com/IamRohitKumar22`)}>
                 <img className="tw-w-11 tw-h-11" src="https://vectorseek.com/wp-content/uploads/2023/07/Twitter-X-Logo-Vector-01-2.jpg" />
