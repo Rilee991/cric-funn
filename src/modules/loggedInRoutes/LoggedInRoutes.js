94d5d525b10f168b25925ad5267dde03a5e28b79
cypher33 <rohitkumar25.rkl@gmail.com>
2023-08-17 13:24:25 +0530

    modified loggedin main component

--- src/modules/loggedInRoutes/LoggedInRoutes.js
+++ src/modules/loggedInRoutes/LoggedInRoutes.js
@@ -1,11 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { BubbleChartOutlined, PublicOutlined, StarBorderOutlined, TimelineOutlined, VpnKey, WhatshotOutlined, 
-    ControlCamera, LocalPlay, AlternateEmail
-} from '@material-ui/icons';
+import { AlternateEmail } from '@material-ui/icons';
 import { find } from 'lodash';
 import moment from 'moment';
 import Confetti from 'react-confetti';
+import { IoMdLogOut } from 'react-icons/io';
+import { SiTencentqq } from 'react-icons/si';
+import { GiStarSwirl } from 'react-icons/gi';
+import { RiDashboard2Line } from 'react-icons/ri';
+import { FaHistory, FaMedal, FaSolarPanel } from 'react-icons/fa';
+import { MdOutlineQueryStats } from 'react-icons/md';
+
+// http://api.weatherstack.com/current?access_key=f44f0188e69910c683149ae9d0e413b3&query=Rourkela
 
 import NotFoundError from '../../components/common/NotFoundError';
 import { Header, SideNavbar, Notifications, Home, MyBets, MyStats, GlobalStats, PointsTable, ControlPanel, Legends } from './index';
@@ -50,49 +56,49 @@ const LoggedInRoutes = () => {
         </div>,
         to: "/",
         subText: `${points} Points`,
-        icon: <LocalPlay />,
+        icon: <SiTencentqq className="tw-h-6 tw-w-6" />,
         component: <Home handleSelectedNav={handleSelectedNav} />
     }, {
         name: "My Bets",
         to: "/my-bets",
-        icon: <TimelineOutlined />,
+        icon: <FaHistory className="tw-h-6 tw-w-6" />,
         component: <MyBets />
     }, {
         name: "My Stats",
         to: "/my-stats",
-        icon: <BubbleChartOutlined />,
+        icon: <RiDashboard2Line className="tw-h-6 tw-w-6" />,
         component: <MyStats />
     }, {
         name: "Global Stats",
         to: "/global-stats",
-        icon: <PublicOutlined />,
+        icon: <MdOutlineQueryStats className="tw-h-6 tw-w-6" />,
         subText: isGlobalStatsDisabled ? "Enabling on 1st May" : "",
         disabled: isGlobalStatsDisabled,
         component: <GlobalStats />
     }, {
         name: "Points Table",
         to: "/points-table",
-        icon: <StarBorderOutlined />,
+        icon: <FaMedal className="tw-h-6 tw-w-6" />,
         component: <PointsTable />
     }, {
         name: "Hall of Fame",
         to: "/hall-of-fame",
-        icon: <WhatshotOutlined />,
+        icon: <GiStarSwirl className="tw-h-6 tw-w-6" />,
         component: <Legends />
     }, {
         name: "Control Panel",
         to: "/control-panel",
-        icon: <ControlCamera />,
+        icon: <FaSolarPanel className="tw-h-6 tw-w-6" />,
         component: <ControlPanel />
     }, {
         name: "Logout",
         to: "/",
         onClick: () => logout(),
-        icon: <VpnKey />
+        icon: <IoMdLogOut className="tw-h-7 tw-w-7" />
     }].map((item, idx) => ({ ...item, id: idx+1 }));
 
     return (
-        <div className="tw-flex tw-flex-col tw-bg-[#8393a0]">
+        <div className="tw-flex tw-flex-col tw-bg-blue-100">
             {isBday && toggleConfetti && <Confetti width={width} height={height + scrollY} className="tw-z-[10000]" numberOfPieces={500} />}
             {isBday && <BirthdayModal claimReward={claimReward} isRewardClaimed={isRewardClaimed} width={width} open={openBdayModal} closeDialog={() => setOpenBdayModal(false)} />}
             <div>
@@ -119,7 +125,7 @@ const LoggedInRoutes = () => {
                         notifications={notifications}
                     />
                 </div>
-                <div className={`tw-pt-16 tw-py-6 md:tw-px-14 tw-w-full tw-min-h-screen`}>
+                <div className={`tw-py-6 md:tw-px-14 tw-w-full tw-min-h-screen`}>
                     <Switch>
                         <Route exact path="/">
                             <Home handleSelectedNav={handleSelectedNav} />
