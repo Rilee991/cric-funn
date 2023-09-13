50f70892dc4221e9f73dd0965f06d33e25bcdfcc
cypher33 <rohitkumar25.rkl@gmail.com>
2023-09-13 23:15:33 +0530

    font change in bet card

--- src/modules/loggedInRoutes/MyBets/BetCard.js
+++ src/modules/loggedInRoutes/MyBets/BetCard.js
@@ -10,14 +10,14 @@ function BetCard(props) {
     const { betTime = "", team1, team2, selectedTeam, selectedPoints, team1Abbreviation, team2Abbreviation, team1Logo, team2Logo } = bet;
     
     return (
-		<Card style={{ boxShadow: "5px 5px 20px"}} className="tw-mt-2 tw-mb-10 xl:tw-w-[70%] md:tw-w-[90%] tw-rounded-[40px]">
+		<Card style={{ boxShadow: "5px 5px 20px"}} className="tw-mt-2 tw-mb-10 tw-rounded-[40px]">
 			<CardActionArea>
 				<CardContent>
 					<MatchPic posterSrc="single" team1Logo={team1Logo} team2Logo={team2Logo} mobileView={mobileView}/>
-					<Typography gutterBottom variant="overline" style={{fontSize: 20, lineHeight: "28px", }} component="h2">
+					<Typography gutterBottom className="tw-font-noto" style={{fontSize: 20, lineHeight: "28px", }} component="h2">
 						<b>{team1} VS. {team2}</b>
 					</Typography>
-					<Typography variant="overline" style={{fontSize: 13}} color="textSecondary" component="p">
+					<Typography className="tw-font-noto" style={{fontSize: 20}} color="textSecondary" component="p">
 						Betting Time: <b>{betTime ? moment.unix(betTime.seconds).format("LLL") : "NA"}</b><br/>
 						Betting Team: <b>{selectedTeam}</b><br/>
 						Betting Points: <b>{selectedPoints}</b>
@@ -25,7 +25,7 @@ function BetCard(props) {
 				</CardContent>
 			</CardActionArea>
 			<Alert style={{ alignItems: "center" }} severity={bet.severity} variant="filled" className="tw-rounded-[40px]">
-				<Typography gutterBottom variant="button" style={{fontSize: 15, lineHeight: "28px", }} component="p">
+				<Typography gutterBottom variant="button" className="tw-font-noto" style={{fontSize: 15, lineHeight: "28px", }} component="p">
 					<b>Bet Status: {bet.message}, {bet.affectedPts} POINTS</b>
 				</Typography>
 			</Alert>
