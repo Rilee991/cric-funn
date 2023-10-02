45797368bad02a51d84858d3c938cf51cbc2df77
cypher33 <rohitkumar25.rkl@gmail.com>
2023-10-02 12:30:49 +0530

    changed comp bar height

--- src/components/common/ComparisionBar.js
+++ src/components/common/ComparisionBar.js
@@ -5,7 +5,7 @@ const ComparisionBar = (props) => {
     const { className = "", color1 = "indigo", width1 = "50%", bgImg1, text1 = "T1", fontSize, color2 = "blue", width2 = "50%", bgImg2, text2 = "T2" } = props;
 
     return (
-        <div style={{ background: color1 }} className={`${className} tw-h-[5vh] tw-rounded-[30px] tw-mt-2 tw-text-white tw-flex`}>
+        <div style={{ background: color1 }} className={`${className} tw-h-[10vh] tw-rounded-[30px] tw-mt-2 tw-text-white tw-flex`}>
             <div style={{ backgroundColor: color1, width: width1, backgroundImage: `url(${bgImg1})`, backgroundSize: "cover", backgroundPosition: "center 40%", backgroundBlendMode: "multiply" }} className="tw-rounded-[30px] tw-flex tw-items-center tw-justify-center">
             <Typography style={{fontSize: fontSize || 16, textShadow: "0 0 3px #0e0101, 0 0 5px #e7e7e9", fontFamily:"Noto Sans"}} component="p">
                 <b>{`${parseFloat(width1) == 0 ? "" : text1}`}</b>
